eefedf771f2bbfc43656294f0e0fc055f376956d
Marc Trankalis <Marc.Trankalis@haw-hamburg.de>
2021-05-30 20:40:51 +0200

    initial

---
+++ src/main/java/choutran/neo4js/Neo4jsApplication.java
@@ -0,0 +1,13 @@
+package choutran.neo4js;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/choutran/neo4js/Neo4jsApplicationTests.java
@@ -0,0 +1,13 @@
+package choutran.neo4js;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
